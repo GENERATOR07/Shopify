7a35af2602b2d88fc636e45392d61196d6ad74ce
GENERATOR07 <1998shubhamrex@gmail.com>
2022-11-12 11:50:03 +0530

    addes search functionality

--- Shopify_Clone/Components/search/Search.jsx
+++ Shopify_Clone/Components/search/Search.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import { useState, useContext } from "react";
 import { MagnifyingGlass } from "phosphor-react";
+import ProductContext from "../../context/ProductContext";
 
 export default function Search() {
+  const [search, setSearch] = useState("");
+  const { dispatch } = useContext(ProductContext);
+  const handelSearch = () => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({
+          type: "search",
+          payload: { data, search, label: "Search Results" },
+        });
+      });
+  };
+
   return (
     <div className=" flex items-center relative left-24 ">
       <input
         className="h-10 w-96 text-center rounded-l-md"
         placeholder="search"
+        onChange={(e) => setSearch(e.target.value)}
       />
-      <div className="p-2 h-10 bg-orange-400  text-black rounded-r-md">
-        <button>
-          <MagnifyingGlass size={24} weight="regular" />
-        </button>
-      </div>
+      <button
+        onClick={handelSearch}
+        className="p-2 h-10 bg-orange-400  text-black rounded-r-md"
+      >
+        <MagnifyingGlass size={24} weight="regular" />
+      </button>
     </div>
   );
 }
--- Shopify_Clone/reducers/productReducer.js
+++ Shopify_Clone/reducers/productReducer.js
@@ -1,3 +1,24 @@
+const searchResults = (payload) => {
+  console.log(payload.data, "yahi hai");
+  let search = payload.search.toLowerCase();
+
+  let result = payload.data.filter((product) => {
+    let { title, description, category } = product;
+    title = title.toLowerCase();
+    description = description.toLowerCase();
+    category = category.toLowerCase();
+    let value =
+      title.includes(search) ||
+      description.includes(search) ||
+      category.includes(search);
+    return value;
+  });
+  console.log(result);
+  return {
+    productData: [...result],
+    label: `Search Results for ${payload.search}`,
+  };
+};
 const productReducer = (product, action) => {
   switch (action.type) {
     case "getProducts":
@@ -5,6 +26,8 @@ const productReducer = (product, action) => {
         productData: [...action.payload.data],
         label: action.payload.label,
       };
+    case "search":
+      return searchResults(action.payload);
 
     default:
       return product;
